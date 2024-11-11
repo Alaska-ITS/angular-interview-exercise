5d27e8921fe7b00f5954aba4aa3da0f55b682341
IngJuanPalomo <juan_ca_88@hotmail.com>
2024-11-11 15:28:39 -0600

    add WeatherService to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherService } from './weather.service';
 
 @NgModule({ declarations: [
         AppComponent
     ],
     bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi()),WeatherService] })
 export class AppModule { }
