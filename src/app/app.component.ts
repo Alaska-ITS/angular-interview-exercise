8e6121f14330250cb39bf07f6f2620f72e9f0027
IngJuanPalomo <juan_ca_88@hotmail.com>
2024-11-11 14:16:44 -0600

    add call services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,29 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor() {
+  stations: any[] = [];
+  selectedStation: any = null;
+  temperature: string | null = null;
+
+  constructor(private weatherService: WeatherService) {
 
   }
 
   ngOnInit(): void {
-    
+    this.loadStations();
   }
 
+  loadStations(): void {
+    this.weatherService.getStations().subscribe((data) => {
+      this.stations = data.features;
+    });
+  }
+
+  onSelectStation(station: any): void {
+    this.selectedStation = station;
+    this.weatherService.getTemperature(station.properties.stationIdentifier).subscribe((data) => {
+      const temperature = data.features[0].properties?.temperature?.value;
+      this.temperature = temperature ? `${temperature} °F` : 'Temperature not available';
+    });
+  }
 }
