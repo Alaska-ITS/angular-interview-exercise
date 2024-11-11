d39556d0eead9866d6a44578aee4c5698afd07c8
IngJuanPalomo <juan_ca_88@hotmail.com>
2024-11-11 13:25:25 -0600

    call API

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  private stationsUrl = 'https://api.weather.gov/stations?limit=100';
+  private observationsUrl = 'https://api.weather.gov/stations/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getStations(): Observable<any> {
+    return this.http.get<any>(this.stationsUrl);
+  }
+
+  getTemperature(stationId: string): Observable<any> {
+    return this.http.get<any>(`${this.observationsUrl}${stationId}/observations?limit=1`);
+  }
 }
