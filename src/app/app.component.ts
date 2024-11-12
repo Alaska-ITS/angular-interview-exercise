1936d67743a919541a61ce8c364ab0ee5a32a35d
IngJuanPalomo <juan_ca_88@hotmail.com>
2024-11-12 16:31:51 -0600

    add paginator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { WeatherService } from './weather.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,11 @@ export class AppComponent implements OnInit {
   stations: any[] = [];
   selectedStation: any = null;
   temperature: string | null = null;
+  paginatedStations: any[] = [];
+  pageSize = 10;
+  currentPage = 0;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private weatherService: WeatherService) {
 
@@ -23,6 +29,7 @@ export class AppComponent implements OnInit {
   loadStations(): void {
     this.weatherService.getStations().subscribe((data) => {
       this.stations = data.features;
+      this.updatePaginatedStations();
     });
   }
 
@@ -32,5 +39,16 @@ export class AppComponent implements OnInit {
       const temperature = data.features[0].properties?.temperature?.value;
       this.temperature = temperature ? `${temperature} °F` : 'Temperature not available';
     });
+  }
+
+  updatePaginatedStations(): void {
+    const startIndex = this.currentPage * this.pageSize;
+    this.paginatedStations = this.stations.slice(startIndex, startIndex + this.pageSize);
+  }
+
+  handlePageEvent(event: PageEvent): void {
+    this.pageSize = event.pageSize;
+    this.currentPage = event.pageIndex;
+    this.updatePaginatedStations();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherService } from './weather.service';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({ declarations: [
         AppComponent
     ],
     bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi()),WeatherService] })
+        AppRoutingModule,MatPaginatorModule], providers: [provideHttpClient(withInterceptorsFromDi()),WeatherService, provideAnimationsAsync()] })
 export class AppModule { }
